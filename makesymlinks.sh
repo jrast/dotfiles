4a1a1d681f12318db178b23d9bd196f6191e88aa
Jürg Rast <juergr@gmail.com>
2013-01-27 17:14:57 +0100

    Nochmals änderungen am instal script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -3,24 +3,22 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 #########################
 
-dir=~/dotfiles
-olddir=~/dotfiles_old
+homedir=~
 
 while getopts u: opt; do
     case "$opt" in
-        u) dir=/home/$OPTARG/dotfiles; olddir=/home/$OPTARG/dotfiles_old;;
+        u) homedir=/home/$OPTARG;;
     esac
 done
+
+dir=$homedir/dotfiles
+olddir=$homedir/dotfiles_old
 
 # Create backup folder
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...Done"
 
-# Change to the dotfiles dir
-echo "Changing to the $dir directory"
-cd $dir
-echo "...Done"
 
 # Move and create Symlinks
 for file in $dir/*; do
@@ -28,11 +26,11 @@ for file in $dir/*; do
     if [ $name = "README.md" -o $name = "makesymlinks.sh" -o $name = ".gitmodules" -o $name = ".gitignore" ]; then
         continue
     fi
-    if [ -e $dir/.$name ]; then
+    if [ -e $homedir/.$name ]; then
         echo "Moving .$name from ~ to $olddir"
-        mv $dir/.$name $olddir/
+        mv $homedir/.$name $olddir/
     fi
     echo "Create symlink to $file in home directory."
-    ln -s $file $dir/.$name
+    ln -s $file $homedir/.$name
 done
 
