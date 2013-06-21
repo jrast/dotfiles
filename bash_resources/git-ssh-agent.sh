eefcc06e0ef6a33ee29ec3d9bf9aa125f1fcbf71
Jürg Rast <juergr@gmail.com>
2013-06-21 15:33:06 +0200

    SSH: Agent nur starten wenn noch keiner läuft

--- bash_resources/git-ssh-agent.sh
+++ bash_resources/git-ssh-agent.sh
@@ -1,4 +1,8 @@
 #!bash
+if [ "$SSH_AUTH_SOCK" != "" ]; then
+    echo "SSH-Agent allready running"
+    return
+fi
 . .agent > /dev/null
 ps -p $SSH_AGENT_PID | grep ssh-agent > /dev/null || {
     ssh-agent > .agent
