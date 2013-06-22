f28565c213817127c2c18cd087aa3cfb12501fc1
Jürg Rast <juergr@gmail.com>
2013-06-22 13:36:39 +0200

    SSH-Agent start verbessert und kommentiert

--- bash_resources/ssh-agent.sh
+++ bash_resources/ssh-agent.sh
@@ -1,9 +1,16 @@
 #!bash
+# Ist schon ein Socket definiert?
 if [ "$SSH_AUTH_SOCK" != "" ]; then
+    # Ok, Script verlassen
     return
 fi
+
 . .agent > /dev/null
 ps -p $SSH_AGENT_PID | grep ssh-agent > /dev/null || {
     ssh-agent > .agent
     . .agent > /dev/null
 }
+# Add Key if necessary
+if [ `ssh-add -l | grep id_rsa | wc -l` = 0 ]; then
+    ssh-add
+fi
